96aac06789839a4d918d28ba206a7da6c87d35e1
Edward Arechwa <e.arechwa@gmail.com>
2022-03-20 07:59:00 +0100

    add alias

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
+const path = require("path");
+
 const nextConfig = {
   reactStrictMode: true,
-}
+  webpack: (config) => {
+    config.resolve.alias["@"] = path.resolve(__dirname);
 
-module.exports = nextConfig
+    return config;
+  },
+};
+
+module.exports = nextConfig;
